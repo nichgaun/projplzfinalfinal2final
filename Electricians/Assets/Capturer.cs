9dcb0293ddc40c3b09d3380722c6423f8e7a54a8
Thomas Rupp <tmrupp@umich.edu>
2018-03-17 00:14:26 -0400

    Added the capturer/capturable interaction

---
+++ Electricians/Assets/Capturer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Capturer : MonoBehaviour {
+    GameObject capturable = null;
+
+    public void CanCapture (GameObject c, bool b) {
+        if (c != null) {
+            if (b)
+                capturable = c;
+            else
+                capturable = null;
+        }
+    }
+
+    void Update() {
+        if (Input.GetKeyDown("space"))
+            Capture();
+        if (Input.GetKeyUp("space"))
+            Uncapture();
+    }
+
+    void Uncapture() {
+        capturable.GetComponent<Capturable>().GetUncaptured(this);
+    }
+
+    void Capture () {
+        capturable.GetComponent<Capturable>().GetCaptured(this);
+    }
+}
