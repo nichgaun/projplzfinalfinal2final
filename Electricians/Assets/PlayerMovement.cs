6474816a97fb001843a805c7f7186e764db07453
U-LAPTOP-KSJORU9T\Nicholas <nichgaun@umich.edu>
2018-03-17 12:23:17 -0400

    move jump

--- Electricians/Assets/PlayerMovement.cs
+++ Electricians/Assets/PlayerMovement.cs
@@ -6,7 +6,13 @@ public class PlayerMovement : MonoBehaviour {
 
 	Rigidbody2D rb;
 
-	public float move_speed = 1f;
+	public float move_speed = 5f;
+	public float acceleration = 35f;
+
+	public float jump_height = 1f;
+	public float jump_time = 1f;
+
+	bool grounded = false;
 
 	// Use this for initialization
 	void Start () {
@@ -15,23 +21,49 @@ public class PlayerMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		
+
+		//movement
 		Vector2 DirectionalInput = Vector2.zero;
-		if (Input.GetKeyDown(KeyCode.D)) {
+		if (Input.GetKey(KeyCode.D)) {
 			DirectionalInput += new Vector2(1, 0);
 		}
-		if (Input.GetKeyDown(KeyCode.S)) {
+		if (Input.GetKey(KeyCode.S)) {
 			DirectionalInput += new Vector2(0, 0);
 		}
-		if (Input.GetKeyDown(KeyCode.A)) {
+		if (Input.GetKey(KeyCode.A)) {
 			DirectionalInput += new Vector2(-1, 0);
 		}
-		if (Input.GetKeyDown(KeyCode.W)) {
+		if (Input.GetKey(KeyCode.W)) {
 			DirectionalInput += new Vector2 (0, 0);
 		}
-		rb.AddForce (DirectionalInput * move_speed);
+		float target_speed = DirectionalInput.x * move_speed;
+		float diff = target_speed - rb.velocity.x;
+		float step = Mathf.Sign (diff) * Mathf.Min (Mathf.Abs (diff), acceleration * Time.deltaTime);
 
+		rb.velocity += new Vector2 (step, 0);
+
+		//gravity and jump
 		Vector2 velocity = rb.velocity;
+
+		float jump_speed = 2 * jump_height / jump_time;
+		float gravity = -2 * jump_height / (jump_time * jump_time);
+		if (!Input.GetKey (KeyCode.Space) && velocity.y > 1e-3) {
+			gravity *= 4;
+		}
+		velocity.y += gravity * Time.deltaTime;
+
+		if (Input.GetKeyDown (KeyCode.Space) && grounded) {
+			velocity.y = jump_speed;
+		}
+
 		rb.velocity = velocity;
+
+		grounded = false;
+	}
+
+	void OnCollisionStay2D (Collision2D collision) {
+		if (collision.contacts [0].normal.y > 0.7) {
+			grounded = true;
+		}
 	}
 }
