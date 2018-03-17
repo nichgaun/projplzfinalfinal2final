988526db922a5df79edda9ff73723b949ba27eb7
Thomas Rupp <tmrupp@umich.edu>
2018-03-17 00:59:39 -0400

    updated some stuff with resources

--- Electricians/Assets/Capturable.cs
+++ Electricians/Assets/Capturable.cs
@@ -6,6 +6,13 @@ public class Capturable : MonoBehaviour {
 
     Capturer owner;
 
+    public enum ResourceType {Computer, Outlet};
+    public ResourceType myType;
+
+    public ResourceType GetResource () {
+        return myType;
+    }
+
     void OnTriggerEnter2D (Collider2D other) {
         if (other.GetComponent<Capturer> () != null)
             other.GetComponent<Capturer>().CanCapture(gameObject, true);
@@ -17,12 +24,15 @@ public class Capturable : MonoBehaviour {
     }
 
     public void GetCaptured (Capturer other) {
+        if (owner != null && other != owner)
+            owner.Uncapture(gameObject);
+
         owner = other;
         GetComponent<SpriteRenderer>().color = Color.blue;
     }
 
     public void GetUncaptured(Capturer other) {
         owner = null;
-        GetComponent<SpriteRenderer>().color = Color.red;
+        GetComponent<SpriteRenderer>().color = Color.white;
     }
 }
--- Electricians/Assets/Capturer.cs
+++ Electricians/Assets/Capturer.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class Capturer : MonoBehaviour {
     GameObject capturable = null;
+    List<GameObject> captured = new List<GameObject>();
+    int outlets, bitcoin;
+
+    private void Start()
+    {
+        outlets = 0;
+        bitcoin = 0;
+    }
 
     public void CanCapture (GameObject c, bool b) {
         if (c != null) {
@@ -14,18 +22,32 @@ public class Capturer : MonoBehaviour {
         }
     }
 
-    void Update() {
+    void Update () {
         if (Input.GetKeyDown("space"))
             Capture();
-        if (Input.GetKeyUp("space"))
-            Uncapture();
+
+        if (Input.GetKeyDown("c"))
+            Debug.Log("Bitcoin: " + bitcoin + " Outlets: " + outlets);
+        //if (Input.GetKeyUp("space"))
+        //    Uncapture();
     }
 
-    void Uncapture() {
-        capturable.GetComponent<Capturable>().GetUncaptured(this);
+    bool IsOutlet (GameObject c) {
+        return c.GetComponent<Capturable>().GetResource() == Capturable.ResourceType.Outlet;
+    }
+
+    public void Uncapture(GameObject other) {
+        if (IsOutlet(other))
+            outlets--;
+        captured.Remove(other);
     }
 
     void Capture () {
-        capturable.GetComponent<Capturable>().GetCaptured(this);
+        Capturable c = capturable.GetComponent<Capturable>();
+        c.GetCaptured(this);
+        captured.Add(capturable);
+
+        if (IsOutlet(capturable))
+            outlets++;
     }
 }
