46e15cfbe35bdd42e9ebeeca7caec27913e42c20
U-LAPTOP-KSJORU9T\Nicholas <nichgaun@umich.edu>
2018-03-17 13:22:14 -0400

    stairs

---
+++ Electricians/Assets/StairWalker.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StairWalker : MonoBehaviour {
+
+	public bool going_up = false, going_down = false;
+	
+	// Update is called once per frame
+	void Update () {
+		going_up = Input.GetKey (KeyCode.W);
+		going_down = Input.GetKey (KeyCode.S);
+	}
+}
---
+++ Electricians/Assets/Stairs.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stairs : MonoBehaviour {
+
+	public GameObject up_location;
+	public GameObject down_location;
+
+	void OnTriggerStay2D (Collider2D other) {
+		StairWalker sw = other.gameObject.GetComponent<StairWalker> ();
+
+		if (up_location != null && sw.going_up) {
+			//move to uplocation
+			other.transform.position = up_location.transform.position;
+		}
+		else if (down_location != null && sw.going_down) {
+			//move to downlocation
+			other.transform.position = down_location.transform.position;
+		}
+	}
+}
