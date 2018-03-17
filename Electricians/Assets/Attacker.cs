a9da9b43f3cd065b2d04ac0497afc8c6b2f5b7ba
Clay Connors <connors.clay@gmail.com>
2018-03-17 02:03:45 -0400

    Attacking

---
+++ Electricians/Assets/Attacker.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attacker : MonoBehaviour {
+	public GameObject weapon;
+
+	void OnEnable() {
+		weapon = Instantiate(weapon, gameObject.transform, false);
+		weapon.GetComponent<BoxCollider2D>().enabled = false;
+	}
+
+    void Update() {
+		if (Input.GetKeyDown("space")) {
+			StartCoroutine(fireWeapon());
+		}
+    }
+
+	IEnumerator fireWeapon() {
+		weapon.GetComponent<BoxCollider2D>().enabled = true;
+		yield return new WaitForSeconds(1);
+		weapon.GetComponent<BoxCollider2D>().enabled = false;
+	}
+}
