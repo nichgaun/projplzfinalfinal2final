14396b6f97989d6a30b272a6bd861d337117daa2
U-LAPTOP-KSJORU9T\Nicholas <nichgaun@umich.edu>
2018-03-17 22:04:57 -0400

    shit

--- Electricians/Assets/Scripts/StairWalker.cs
+++ Electricians/Assets/Scripts/StairWalker.cs
@@ -7,6 +7,7 @@ public class StairWalker : MonoBehaviour {
 	ControllerController cc;
 
 	public bool going_up = false, going_down = false;
+	public bool recentered = false;
 
 	void Start () {
 		cc = GetComponent<ControllerController> ();
@@ -14,7 +15,21 @@ public class StairWalker : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		going_up = Input.GetKey (KeyCode.W) || (cc.controller != null && cc.controller.LeftStickY > 0.5);
-		going_down = Input.GetKey (KeyCode.S) || (cc.controller != null && cc.controller.LeftStickY < -0.5);
+		going_up = false;
+		going_down = false;
+
+
+		if (Input.GetKey (KeyCode.W) || (cc.controller != null && cc.controller.LeftStickY > 0.6) && recentered) {
+			recentered = false;
+			going_up = true;
+		}
+		if (Input.GetKey (KeyCode.S) || (cc.controller != null && cc.controller.LeftStickY < -0.6) && recentered) {
+			recentered = false;
+			going_down = true;
+		}
+
+		if (Mathf.Abs (cc.controller.LeftStickY) < 0.6) {
+			recentered = true;
+		}
 	}
 }
