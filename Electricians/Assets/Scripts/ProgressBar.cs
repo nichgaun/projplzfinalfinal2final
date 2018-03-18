da8a75a9e44747dae8d2aa8235b8edb6e6a9705b
U-LAPTOP-KSJORU9T\Nicholas <nichgaun@umich.edu>
2018-03-18 05:34:19 -0400

    lots of stuff TM

---
+++ Electricians/Assets/Scripts/ProgressBar.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProgressBar : MonoBehaviour {
+
+	Transform fill;
+
+	// Use this for initialization
+	void Start () {
+		fill = transform.Find ("Fill");
+	}
+
+	public void set_fill(float val) {
+		if (val > 1)
+			val = 1;
+		if (val < 0)
+			val = 0;
+
+		Vector3 tmp = fill.localScale;
+		tmp.x = val;
+		fill.localScale = tmp;
+	}
+}
